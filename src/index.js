65c09a68e3b317fb102f260c4eb35b9bcb02a41e
7zYBN <frazo4ka@mail.ru>
2019-10-19 19:17:56 +0300

    feat: add completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function zeros(expression) {
-  // your solution
+  const arr = expression.split('*');
+  
+  let zerosCount = 0;
+  let fivesCount = 0;
+  let twosCount = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+
+    const indexOfFactorialSymbol = arr[i].indexOf('!');
+    const step = arr[i].slice(indexOfFactorialSymbol).length;
+    const number = Number(arr[i].slice(0, indexOfFactorialSymbol));
+
+    for (let j = number % 2 + step; j <= number; j += step) {
+      let factor = j;
+
+      while (factor % 5 === 0 || factor % 2 === 0) {
+
+        if (factor % 5 === 0) {
+          factor /= 5;
+          fivesCount++
+        } else {
+          factor /= 2;
+          twosCount++;
+        }
+        
+      }
+    }
+  }
+
+  zerosCount += Math.min(fivesCount, twosCount);;
+
+  return zerosCount;
 }
